5140164d9dd9e9e8a1cdc7fa68285b28ec61cca5
xinyu3ru <xinyu3ru@users.noreply.github.com>
2016-01-31 17:30:03 +0800

    Create uniqueTimestamp.py

---
+++ uniqueTimestamp.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'rublog'
+import time
+import random
+
+localtime = time.localtime()
+print(localtime)
+today = int(time.strftime("%w"))
+millisec = random.randint(0, 999)
+print(millisec)
+uniqueTimestamp = str(localtime[0])+str(localtime[1]-1)+str(today)+str(localtime[3])+str(localtime[4])+str(millisec)
+print(uniqueTimestamp)
